e8f4e8926430cede3ace8cf272af4754071e9a3c
David Kasilia <83514256+David-Kasilia@users.noreply.github.com>
2022-09-06 01:51:14 +0300

    Part 7 complete react tutorial

--- react-todo-app/src/index.js
+++ react-todo-app/src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
 //component file
-import TodoContainer from './components/TodoContainer';
+import TodoContainer from "./functionBased/components/TodoContainer"
+
 //stylesheet
-import "./App.css"
+import "./functionBased/App.css"
+import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router>
+      <TodoContainer />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
